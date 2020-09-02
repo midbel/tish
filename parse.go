3d9e92906cf95e4f1fe107c60467f8a26e73d8d7
midbel <brun.nicolas21@gmail.com>
2020-09-02 10:19:33 +0200

    parser skeleton

--- parse.go
+++ parse.go
@@ -1,1 +1,38 @@
 package tish
+
+import (
+  "io"
+)
+
+type Parser struct {
+  scan *Scanner
+  curr Token
+  peek Token
+}
+
+func Parse(r io.Reader) (*Parser, error) {
+  s, err := NewScanner(r)
+  if err != nil {
+    return nil, err
+  }
+
+  var p Parser
+  p.scan = s
+  p.next()
+  p.next()
+
+  return &p, nil
+}
+
+func (p *Parser) Parse() error {
+  return nil
+}
+
+func (p *Parser) next() {
+  p.curr = p.peek
+  p.peek = p.scan.Next()
+}
+
+func (p *Parser) isDone() {
+  return p.curr.Type == TokEOF
+}
