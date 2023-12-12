347cd0c74c47de4079b14fd102722a196356345b
midbel <brun.nicolas21@gmail.com>
2023-12-12 21:17:55 +0100

    update cat

--- cmd/classics/cat/main.go
+++ cmd/classics/cat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"errors"
 	"flag"
 	"fmt"
@@ -9,18 +10,21 @@ import (
 )
 
 func main() {
-	limit := flag.Int64("n", 0, "number of bytes")
+	var (
+		limit = flag.Int64("c", 0, "number of bytes")
+		number = flag.Bool("n", false, "print line number")
+	)
 	flag.Parse()
 
 	for _, f := range flag.Args() {
-		if err := cat(f, *limit); err != nil {
+		if err := cat(f, *limit, *number); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 	}
 }
 
-func cat(file string, limit int64) error {
+func cat(file string, limit int64, number bool) error {
 	r, err := os.Open(file)
 	if err != nil {
 		return err
@@ -31,9 +35,21 @@ func cat(file string, limit int64) error {
 	if limit > 0 {
 		rs = io.LimitReader(r, limit)
 	}
+	if number {
+		return printNumber(rs)
+	}
 	_, err = io.Copy(os.Stdout, rs)
 	if errors.Is(err, io.EOF) {
 		err = nil
 	}
 	return err
+}
+
+func printNumber(r io.Reader) error {
+	scan := bufio.NewScanner(r)
+	for i := 1; scan.Scan(); i++ {
+		fmt.Printf("%4d%s", i, scan.Text())
+		fmt.Println()
+	}
+	return scan.Err()
 }
