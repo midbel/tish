ad89a07745a4243f447731b832d74df5f640f077
midbel <brun.nicolas21@gmail.com>
2023-12-18 20:33:31 +0100

    sleep command

---
+++ cmd/classics/sleep/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"time"
+	"flag"
+	"strconv"
+	"fmt"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+	var (
+		sec time.Duration
+		err error
+	)
+	if sec, err = time.ParseDuration(flag.Arg(0)); err != nil {
+		n, err := strconv.Atoi(flag.Arg(0))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "fail to parse argument as number")
+			os.Exit(1)
+		}
+		sec = time.Duration(n) * time.Second
+	}
+
+	time.Sleep(sec)
+}
